41d6f5ef623bebd9b71b21456334891c49f66234
prathmeshthorat <prathmeshthorat06@gmail.com>
2022-05-20 11:41:37 +0530

    okta basic setup

--- estock-market/company-command/src/main/java/com/estockmarket/company/command/CommandApplication.java
+++ estock-market/company-command/src/main/java/com/estockmarket/company/command/CommandApplication.java
@@ -1,34 +1,36 @@
 package com.estockmarket.company.command;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.estockmarket.company.command.api.AddStockPriceCommand;
 import com.estockmarket.company.command.api.CommandHandler;
 import com.estockmarket.company.command.api.DeleteCompanyCommand;
 import com.estockmarket.company.command.api.RegisterComapnyCommand;
 import com.estockmarket.cqrscore.infra.CommandDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class CommandApplication {
 
-	@Autowired
-	private CommandDispatcher commandDispatcher;
+    private final Logger logger = Logger.getLogger(CommandApplication.class.getName());
 
-	@Autowired
-	private CommandHandler commandHandler;
+    @Autowired
+    private CommandDispatcher commandDispatcher;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommandApplication.class, args);
-	}
+    @Autowired
+    private CommandHandler commandHandler;
 
-	@PostConstruct
-	public void registerhandlers() {
-		commandDispatcher.registerHandler(RegisterComapnyCommand.class, commandHandler::handle);
-		commandDispatcher.registerHandler(AddStockPriceCommand.class, commandHandler::handle);
-		commandDispatcher.registerHandler(DeleteCompanyCommand.class, commandHandler::handle);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class, args);
+    }
+
+    @PostConstruct
+    public void registerhandlers() {
+        commandDispatcher.registerHandler(RegisterComapnyCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(AddStockPriceCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(DeleteCompanyCommand.class, commandHandler::handle);
+    }
 }
